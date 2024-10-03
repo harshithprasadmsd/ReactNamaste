7111866011ad4a93a2e6ac722778ba48452e3198
Harshith <harshithprasadmsd@gmail.com>
2024-10-03 12:16:46 +0530

    AppLayout is rendering

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import ReactDOM from "react-dom";
 
-//create a react element
-const headingElement = <h1>this is react element</h1>;
+const AppLayout = () => {
+  return (
+    <div>
+      <h1>app is rendering </h1>
+    </div>
+  );
+};
 
-//create a react component (functional)
-const HeadingComponent = () => (
-  <div>
-    <h1>this is h1 inside fc</h1>
-    headingElement
-    {SubHeading}
-    <SubHeading />
-  </div>
-);
-//create another react component
-const SubHeading = () => <h3>this is subheading</h3>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent></HeadingComponent>);
+root.render(<AppLayout />);

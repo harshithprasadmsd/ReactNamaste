27be209587033656210da7123f19e0b7814cb2c8
Harshith <harshithprasadmsd@gmail.com>
2024-10-08 20:39:52 +0530

    add login ,logOut toggle button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
 import { LOGO_URL } from "../utils/constants";
-const Header = () => (
-  <div className="header">
-    <div className="logoContainer">
-      <img className="logo" src={LOGO_URL}></img>
+import { useState } from "react";
+const Header = () => {
+  const [btnName, setBtnName] = useState("login");
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img className="logo" src={LOGO_URL}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home </li>
+          <li>About Us</li>
+          <li>Contact Us </li>
+          <li>Cart</li>
+          <button
+            class="login"
+            onClick={() => {
+              btnName === "login" ? setBtnName("logOut") : setBtnName("login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home </li>
-        <li>About Us</li>
-        <li>Contact Us </li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;

2dc00450a2a81f8a910b7d057e45c095dd482e45
Harshith <harshithprasadmsd@gmail.com>
2024-10-27 16:35:12 +0530

    added config for tailwind and added css for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,25 @@ const Header = () => {
   const [btnName, setBtnName] = useState("login");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
-      <div className="logoContainer">
-        <img className="logo" src={LOGO_URL}></img>
+    <div className="flex justify-between items-center">
+      <div>
+        <img className="w-40" src={LOGO_URL}></img>
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>onlineStatus:{onlineStatus ? "🟩" : "🟥"}</li>
-          <li>
+      <div>
+        <ul className="flex justify-start">
+          <li className="px-2">onlineStatus:{onlineStatus ? "🟩" : "🟥"}</li>
+          <li className="px-2">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-2">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-2">
             <Link to="/contacts">Contact Us</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-2">Cart</li>
           <button
-            className="login"
+            className="w-10 bg-green-400 "
             onClick={() => {
               btnName === "login" ? setBtnName("logOut") : setBtnName("login");
             }}
